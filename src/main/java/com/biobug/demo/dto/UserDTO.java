0f5644e7a57273532d8f7db3c513e53d0b9129b6
Emmanuel <emmanuelsilano@gmail.com>
2024-11-07 21:43:37 -0500

    se ha modificado UserDTO

--- src/main/java/com/biobug/demo/dto/UserDTO.java
+++ src/main/java/com/biobug/demo/dto/UserDTO.java
@@ -5,7 +5,7 @@ import com.biobug.demo.model.User;
 import java.util.List;
 
 public record UserDTO(
-        Long idUser, String rolUser, String nameUser, String lastName, String emailUser, List<OrderDTO> oders
+        Long idUser, String rolUser, String nameUser, String lastName, String emailUser, List<OrderDTO> orders
 ) {
     public UserDTO(User user){
         this(user.getIdUser(), user.getRolUser(), user.getNameUser(), user.getLastName(), user.getEmailUser(),
